ca3c907e14f8ca317594ead0c765615a5cf83460
Vincent Marshall <vmarshall2006@conestogac.on.ca>
2022-11-27 17:39:28 -0500

    Added contract class used to store contract data as a struct

---
+++ TMS/Model/Contract.cs
@@ -0,0 +1,25 @@
+﻿/*
+ *  FILE            : Contract.cs
+ *  PROJECT         : TMS - Group 7
+ *  PROGRAMMER      : Vincent Marshall
+ *  FIRST VERSION   : 27/11/2022
+ *  DESCRIPTION     :
+ *      This file contains a class that
+ *      represents a customer contract
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.Model {
+    internal class Contract {
+
+        public Contract() {
+        
+        }
+
+        public Contract(string client_name) {
+    }
+}
