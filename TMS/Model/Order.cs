9d1e8c8428db069e361cc3db9c0393ad63937936
Vincent Marshall <vmarshall2006@conestogac.on.ca>
2022-12-04 16:28:05 -0500

    first version of order class file

---
+++ TMS/Model/Order.cs
@@ -0,0 +1,30 @@
+﻿/*
+ *  FILE            : Order.cs
+ *  PROJECT         : TMS - Group 7
+ *  PROGRAMMER      : Vincent Marshall
+ *  FIRST VERSION   : 27/11/2022
+ *  DESCRIPTION     :
+ *      This file contains an order structure
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.Model {
+
+    public enum Status {
+        AwaitingApproval,
+        Schedueled,
+        Delivered,
+        Completed
+    }
+
+    internal class Order {
+
+        public Status OrderStatus {get; set;}
+
+
+    }
+}
