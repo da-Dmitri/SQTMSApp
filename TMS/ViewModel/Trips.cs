198f3d147cf81363b808c79133a24aec0d448fc8
AngelAvilesSil <aavilessil1750@conestogac.on.ca>
2022-12-04 19:56:05 -0500

    Added trips class and city class

---
+++ TMS/ViewModel/Trips.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.ViewModel
+{
+    internal class Trips
+    {
+
+        public int TripID { get; set; }
+        public int OrderID { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+
+
+        public Trips(int tripNumber, int orderNumber, string theCarrier, string theOrigin, string theDestination)
+        {
+            TripID = tripNumber;
+            OrderID = orderNumber;
+            Origin = theOrigin;
+            Destination = theDestination;
+        }
+    }
+}
